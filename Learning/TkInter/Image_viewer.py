8811a48c9577b517d592b76a85708069a73633f1
abhilashTUofficial <abhilashstorm@gmail.com>
2021-06-11 18:25:35 +0530

    update

--- Learning/TkInter/Image_viewer.py
+++ Learning/TkInter/Image_viewer.py
@@ -4,30 +4,62 @@ from PIL import ImageTk,Image
 frame=tkinter.Tk();
 frame.title("Images");
 # frame.geometry("600x600");
-img1=ImageTk.PhotoImage(Image.open('Img/pycharm.png').resize((400,400)));
-img2=ImageTk.PhotoImage(Image.open('Img/python.png').resize((400,400)));
-img3=ImageTk.PhotoImage(Image.open('Img/vscode.png').resize((400,400)));
-img4=ImageTk.PhotoImage(Image.open('Img/pycharm.png').resize((400,400)));
+pyimage1=ImageTk.PhotoImage(Image.open('Img/pycharm.png').resize((400,400)));
+pyimage2=ImageTk.PhotoImage(Image.open('Img/python.png').resize((400,400)));
+pyimage3=ImageTk.PhotoImage(Image.open('Img/vscode.png').resize((400,400)));
+# pyimage4=ImageTk.PhotoImage(Image.open('Img/pycharm.png').resize((400,400)));
 
-imagesList=[img1,img2,img3,img4];
-n=0;
-label=tkinter.Label(image=img1);
+imagesList=[pyimage1,pyimage2,pyimage3];
+label=tkinter.Label(image=imagesList[1]);
 label.grid(row=0,column=0,columnspan=3);
 
-def backward():
-    pass
+def backward(n):
+    global label;
+    global forwardButton;
+    global backwardButton;
+    try:
+        label.grid_forget();
+        label=tkinter.Label(image=imagesList[n-1]);
+        label.grid(row=0,column=0,columnspan=3);
+        forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward(n-1));
+        backwardButton=tkinter.Button(frame,text="<<",command=lambda : backward(n+1));
+        backwardButton.grid(row=1,column=0);
+        forwardButton.grid(row=1,column=2);
+    except:
+        label.grid_forget();
+        label=tkinter.Label(image=imagesList[0]);
+        label.grid(row=0,column=0,columnspan=3);
+        backwardButton=tkinter.Button(frame,text="<<",command=lambda : backward(0));
+        forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward(0));
 
-def forward():
-    label=tkinter.Label(image=imagesList[2]);
-    label.grid(row=0,column=0,columnspan=3);
+        backwardButton.grid(row=1,column=0);
+        forwardButton.grid(row=1,column=2);
     
 
+def forward(n):
+    global label;
+    global forwardButton;
+    global backwardButton;
+    try:
+        label.grid_forget();
+        label=tkinter.Label(image=imagesList[n+1]);
+        label.grid(row=0,column=0,columnspan=3);
+        forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward(n+1));
+        backwardButton=tkinter.Button(frame,text="<<",command=lambda : backward(n-1));
+        backwardButton.grid(row=1,column=0);
+        forwardButton.grid(row=1,column=2);
+    except:
+        label.grid_forget();
+        label=tkinter.Label(image=imagesList[0]);
+        label.grid(row=0,column=0,columnspan=3);
+        backwardButton=tkinter.Button(frame,text="<<",command=backward);
+        forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward(0));
 
+        backwardButton.grid(row=1,column=0);
+        forwardButton.grid(row=1,column=2);
 
-
-
-backwardButton=tkinter.Button(frame,text="<<",command=backward);
-forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward());
+backwardButton=tkinter.Button(frame,text="<<",command=lambda : backward(1));
+forwardButton=tkinter.Button(frame,text=">>",command=lambda : forward(1));
 
 backwardButton.grid(row=1,column=0);
 forwardButton.grid(row=1,column=2);
